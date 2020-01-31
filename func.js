9c4e5e78b9558f610c10cc6f9c1ec2f63ceaa606
nickm324 <nickm@kryptontech.com>
2020-01-31 14:15:22 -0500

    fix error

--- func.js
+++ func.js
@@ -1,5 +1,5 @@
-var x = myFunction(4, 3);   // Function is called, return value will end up in x
+var x = myFunction(4, 3);   // Function is called, return value will end up in x -test function
 
 function myFunction(a, b) {
-  return a * b;             // Function returns the product of a and b
+  return a * b;             // Function returns the product of a and b testing
 }
